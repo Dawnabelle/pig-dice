9b2f139028f4f3f2616e6577748d1129ab798cb7
Ami Cooper, Dawn Mott and Matt Dowdney <dawn@>
2018-04-04 10:09:02 -0700

    add css styling and js hold logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 var player1Score = 0;
 var player2Score = 0;
+var player1Overall = [];
+var player2Overall = [];
 
 function Player (name, score){
   this.name = name;
@@ -24,18 +26,48 @@ function roll2() {
  }
 }
 
+function hold1() {
+  player1Overall.push(player1Score);
+  player1Score = 0;
+}
+
+function hold2() {
+  player2Overall.push(player2Score);
+    player2Score = 0;
+}
+
 $(document).ready(function(){
   $(".p1Roll").click(function(event){
     event.preventDefault();
       roll1();
+      if (player1Score === 0){
+        $("#player-1").hide();
+        $("#player-2").show();
+      }
       $('.p1Score').text(player1Score);
 
   })
   $(".p1Hold").click(function(event){
-
+    event.preventDefault();
+    hold1();
+    $("#player-1").hide();
+    $("#player-2").show();
+    console.log(player1Overall);
   })
   $(".p2Roll").click(function(event){
     event.preventDefault();
-
+    roll2();
+    if (player2Score === 0){
+      $("#player-2").hide();
+      $("#player-1").show();
+    }
+    $('.p2Score').text(player2Score);
   })
-})
+  $(".p2Hold").click(function(event){
+    event.preventDefault();
+    hold2();
+    $("#player-2").hide();
+    $("#player-1").show();
+    console.log(player2Overall);
+  })
+});
